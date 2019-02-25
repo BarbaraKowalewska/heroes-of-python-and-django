e09a66f04983ebb9647d793e518e951d1ca1fbdc
JanBartoszek <bartoszek.jan@gmail.com>
2019-02-25 12:52:34 +0100

    modified settings.py in order to use postgresql

--- hopad/hopad/settings.py
+++ hopad/hopad/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'hopad.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("NAME_HOPAD"),
+        'USER': os.environ.get("USER_HOPAD"),
+        'PASSWORD': os.environ.get("PASSWORD_HOPAD"),
+        'HOST': os.environ.get("HOST")
     }
 }
 
