fc72489948aa3e067dd113f177db41f61ab992b9
Adam <akalinowski44@github.com>
2019-02-28 11:36:29 +0100

    posds template created and handled

--- hopad/forum/views.py
+++ hopad/forum/views.py
@@ -9,8 +9,20 @@ def forum_home(request):
     context = {'categories': categories }
     return render(request,'forum/forum_home.html', context)
 
-def forum_topics(request, name):
-    category_id = Category.objects.get(name = name).id
+def forum_topics(request, category_name):
+    category_id = Category.objects.get(name = category_name).id
     topics = Topic.objects.filter(category = category_id)
-    context = {'topics': topics}
+    context = {
+        'topics': topics,
+        'category_name': category_name
+    }
     return render(request, 'forum/forum_topics.html', context)
+
+
+def forum_posts(request, category_name, topic_id):
+    posts = Post.objects.filter(topic = topic_id)
+    context = {
+        'category_name': category_name,
+        'posts': posts
+        }
+    return render(request, 'forum/forum_posts.html', context)
