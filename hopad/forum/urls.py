8c3c3016607a62e2a7d968ba84fb408735fd1935
JanBartoszek <bartoszek.jan@gmail.com>
2019-03-04 21:43:32 +0100

    created a new class-based view for submiting a new topics

--- hopad/forum/urls.py
+++ hopad/forum/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-
-
 from . import views
 
 app_name = 'forum'
 urlpatterns = [
     path('', views.forum_home, name='forum_home'),
     path('<str:category_name>/topics', views.forum_topics, name='forum_topics'),
+    path('new-topic', views.NewTopicForm.as_view(), name='new_topic'),
     path('<str:category_name>/topics/<int:topic_id>', views.forum_posts, name='forum_posts')
 ]
 
--- hopad/forum/views.py
+++ hopad/forum/views.py
@@ -1,17 +1,18 @@
-from django.shortcuts import render
-
-
+from django.shortcuts import render, redirect
+from django.views import View
 from .models import Category, Topic, Post
+from .forms import NewTopicForm
 
 
 def forum_home(request):
     categories = Category.objects
-    context = {'categories': categories }
-    return render(request,'forum/forum_home.html', context)
+    context = {'categories': categories}
+    return render(request, 'forum/forum_home.html', context)
+
 
 def forum_topics(request, category_name):
-    category_id = Category.objects.get(name = category_name).id
-    topics = Topic.objects.filter(category = category_id)
+    category_id = Category.objects.get(name=category_name).id
+    topics = Topic.objects.filter(category=category_id)
     context = {
         'topics': topics,
         'category_name': category_name
@@ -20,9 +21,32 @@ def forum_topics(request, category_name):
 
 
 def forum_posts(request, category_name, topic_id):
-    posts = Post.objects.filter(topic = topic_id)
+    posts = Post.objects.filter(topic=topic_id)
     context = {
         'category_name': category_name,
         'posts': posts
         }
     return render(request, 'forum/forum_posts.html', context)
+
+
+class NewTopic(View):
+    form_class = NewTopicForm
+    initial = {'key': 'value'}
+    template_name = 'forum/new_topic.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            full_form = form.save(commit=False)
+            full_form.user = request.user
+            full_form.save()
+            return redirect('forum:forum_home')
+        return render(request, self.template_name, {'form': form})
+
+
+
+
