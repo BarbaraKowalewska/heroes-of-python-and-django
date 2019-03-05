c6a583115f5718f519ae5d39f68d8e4156ae86ea
BarbaraKowalewska <baw.kowalewska@gmail.com>
2019-03-05 21:14:43 +0100

    Profile update with picture resized

--- hopad/accounts/views.py
+++ hopad/accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from accounts.forms import UserRegisterForm
+from accounts.forms import UserRegisterForm, ProfileUpdate
 
 
 def register(request):
@@ -11,11 +11,20 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username} You can now log in!')
-            return redirect('accounts:login')
+            return redirect('accounts:profile')
     else:
         form = UserRegisterForm()
     return render(request, 'accounts/register.html', {'form': form})
 
+
 @login_required
 def profile(request):
-    return render(request, 'accounts/profile.html')
+    if request.method == 'POST':
+        form = ProfileUpdate(request.POST, request.FILES, instance=request.user.account)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Your account has been updated')
+            return redirect('accounts:profile')
+    else:
+        form = ProfileUpdate(instance=request.user)
+    return render(request, 'accounts/profile.html', {'form': form})
